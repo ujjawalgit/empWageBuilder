f9bce1d3efd3287869805cad349cc6ed5d4ec8ce
ujjawalgit <ujjawalkumarsingh08@gmail.com>
2021-02-20 22:21:19 +0530

    salary calculation for full time

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,19 +1,27 @@
 public class  EmpWageBuilder{
 
-  public static void main(String []args){
-	
-	//Constants
-	int IS_FULL_TIME=1;
-	
-	//Computation
-	double empCheck= Math.floor(Math.random()*10)%2;
-	
-	if (empCheck==IS_FULL_TIME)
-	    System.out.print("Employee is present");
-	else
-	    System.out.print("Employee is absent");
-	
-	}
+    public static void main(String []args){
 
+	final int IS_FULL_TIME=1;
+        final int WAGE_RATE=20;
+
+        int empHr=0;
+        int salary=0;
+
+	double empCheck= Math.floor(Math.random()*10)%2;
+
+	if (empCheck==IS_FULL_TIME){
+            System.out.print("Employee is present");
+            empHr=8;
+	}
+        
+	 else{
+             System.out.print("Employee is absent");
+             empHr=0;
+            }
+
+        salary=(WAGE_RATE*empHr);
+        System.out.print("\nsalary:"+salary);
+	}
 }
 
